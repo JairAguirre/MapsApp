89ffbb45f17ce1683e1b2ecebc22d98c653f2558
JairAguirre <aguirre.jair@gmail.com>
2021-11-24 00:00:00 -0500

    Uploaded full project

---
+++ app/src/main/java/com/example/reto_1/MapsActivity.java
@@ -0,0 +1,88 @@
+package com.example.reto_1;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.chip.ChipGroup;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    private FloatingActionButton button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+
+        if(status == ConnectionResult.SUCCESS) {
+
+
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }else{
+
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status,(Activity) getApplicationContext(), 10);
+        }
+
+            button = findViewById(R.id.floatingActionButton2);
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(3.42158, -76.5205);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)).draggable(true));
+
+        float zoom = 14;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoom));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        button.setOnClickListener(
+
+                (V)->{
+
+                    Toast.makeText(MapsActivity.this,"Versión 1.0.", Toast.LENGTH_SHORT).show();
+                }
+        );
+
+    }
+
+
+    public void addMaker(GoogleMap map, double posa, double posb, String nombre){
+
+
+        //map.addMarker(new MarkerOptions().position(new LatLng()).title(nombre) );
+    }
+}
